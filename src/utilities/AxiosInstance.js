6d367ace9ea4b084b0430e98b1779e56e05c60ec
Logeshpv2001 <logeshkarthi782@gmail.com>
2025-03-04 13:41:24 +0530

    axios changed

--- src/utilities/AxiosInstance.js
+++ src/utilities/AxiosInstance.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   // baseURL: "http://localhost:7799",
+  baseURL: "https://qsistest.quantumsharq.com/api",
   // baseURL: "https://svpc.qsisphysio.com/api",
-  baseURL: "https://svpc-backend-v2gw.onrender.com" /*Render Link */,
+  // baseURL: "https://svpc-backend-v2gw.onrender.com" /*Render Link */,
 });
 
 export default AxiosInstance;
